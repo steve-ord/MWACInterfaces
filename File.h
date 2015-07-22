2de358602377d7d2d1ef17d752a8cacdf78e01ec
Stephen Ord <steve.ord@gmail.com>
2015-07-22 22:37:28 +0800

    Changed the doxygen author tag

--- File.h
+++ File.h
@@ -1,6 +1,6 @@
 /** 
  * @file:   File.h
- * @author: sord
+ * @author: Stephen Ord
  * @brief Abstract base class for files used as interfaces to the MWA Correlator 
  * @date 22 July 2015, 9:33 PM
  * 
