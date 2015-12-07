bd2bc6bfb0a49b66ed1a888432b321e112f82e83
Stephen Ord <steve.ord@gmail.com>
2015-12-07 21:31:40 +0800

    fixed the cppcheck in cmake

--- cmake/Modules/FindCppcheck.cmake
+++ cmake/Modules/FindCppcheck.cmake
@@ -3,6 +3,7 @@ find_program(CPPCHECK_EXECUTABLE cppcheck )
 if (CPPCHECK_EXECUTABLE)
     SET(CPPCHECK_FOUND TRUE)
     mark_as_advanced(CPPCHECK_EXECUTABLE)
+    find_program(CPPCHECK-HTML_EXECUTABLE cppcheck-htmlreport)
 
 endif (CPPCHECK_EXECUTABLE)
 
